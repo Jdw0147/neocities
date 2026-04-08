b50c010740d5ec787ef60005ef7549118302c479
David Wheeler <jdw0147@auburn.edu>
2026-04-08 08:07:25 -0500

    Random button functionality

---
+++ random.js
@@ -0,0 +1,17 @@
+const randomLinks = [
+  { url: 'music.html', name: 'Music Page' },
+  { url: 'sotd.html', name: 'Song of the Day' }
+  // Add more links here as you build the site
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+  const randomLink = document.querySelector('.icon-link-item:first-child .icon-link');
+  
+  if (randomLink) {
+    randomLink.addEventListener('click', (e) => {
+      e.preventDefault();
+      const randomPage = randomLinks[Math.floor(Math.random() * randomLinks.length)];
+      window.location.href = randomPage.url;
+    });
+  }
+});
